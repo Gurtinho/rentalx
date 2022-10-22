aa00ab087c41b0765da677e7172639ec03b361ef
gurtinho <gustavolitter@gmail.com>
2022-10-22 19:21:26 -0300

    adicionado pasta models, novos routes e uma classe category para tipar as categorias e criar automaticamente o uuid do id.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 const app = express();
 app.use(express.json());
+app.use('/categories', categoriesRoutes);
 
 app.get('/', (request, response) => {
-    return response.status(201).json({ name: 'gustavo' });
+    return response.status(201).send();
 });
 
 app.listen(3333, () => console.log('server is running'));
